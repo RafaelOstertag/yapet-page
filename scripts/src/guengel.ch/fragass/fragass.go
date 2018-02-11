3e3e2e61cbe6d627e4ca3da6bb022d126f45917b
Rafael Ostertag <rafi@guengel.ch>
2018-02-11 13:33:26 +0100

    Added comment.

--- scripts/src/guengel.ch/fragass/fragass.go
+++ scripts/src/guengel.ch/fragass/fragass.go
@@ -1,3 +1,19 @@
+/*
+Create a HTML file from a template and a fragment. The template is
+a Go template having {{.Title}} and {{.Content}} variables. {{.Title}}
+is the HTML page title, the {{.Content}} is placed verbatim into the template.
+
+The fragment is a XML file as shown below.
+
+     <?xml version="1.0" encoding="utf-8"?>
+     <fragment>
+        <title>Page Title</title>
+        <content><![CDATA[<h1>Content</h1> <p>yada yada</p>]]></content>
+     </fragment>
+
+The HTML is sent to stdout.
+*/
+
 package main
 
 import (
