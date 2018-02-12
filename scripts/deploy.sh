85262a39b2aa78b571b3cb7c22d5373c11928e10
Rafael Ostertag <rafi@guengel.ch>
2018-02-12 18:11:51 +0100

    Introduced variables, cd to target dir before sending.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,13 @@
 #
 # Deploy built page to eventhorizon.dmz.kruemel using sftp.
 
+REMOTE_USER=yapet-deploy
+REMOTE_HOST=eventhorizon.dmz.kruemel.home
 REMOTE_BASE=/var/www/jails/yapet/usr/local/www/apache24/data
 
 cd public_html
-sftp yapet-deploy@eventhorizon.dmz.kruemel.home <<EOF
+sftp ${REMOTE_USER}@${REMOTE_HOST} <<EOF
+cd ${REMOTE_BASE}
 put -r .
 bye
 EOF
